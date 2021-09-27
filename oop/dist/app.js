d7c325fdf94fb626ed3e74f31ff7a4ba3ba6bcd8
ext.joao.g.monteiro <ext.joao.g.monteiro@farfetch.com>
2021-09-27 16:35:27 +0100

    OOP

---
+++ oop/dist/app.js
@@ -0,0 +1,23 @@
+"use strict";
+class Department {
+    constructor(n) {
+        this.employees = [];
+        this.name = n;
+    }
+    describe() {
+        console.log("Department: ", this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department("Accounting");
+accounting.describe();
+accounting.addEmployee("João");
+accounting.addEmployee("Rita");
+accounting.printEmployeeInformation();
+//# sourceMappingURL=app.js.map
